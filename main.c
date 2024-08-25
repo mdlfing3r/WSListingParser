f985066fa8a1436a0d09f3559f35143bfed95c2b
Alexandr <mdlfing3r@gmail.com>
2024-08-26 00:34:07 +0300

    Work is done !

--- main.c
+++ main.c
@@ -25,6 +25,7 @@ extern int pcap_findalldevs(pcap_if_t**, char*);
 
 typedef struct params{
     const char* name;
+    char *descr;
     char *value; 
 } params_t;
 
@@ -44,21 +45,21 @@ int main(const int argc, const char** argv)
 
     params_t paramsList[5] = 
     {   
-        {pathPrefix,' '},
-        {srcAddrPrefix,' '},
-        {dstAddrPrefix,' '},
-        {srcPortPrefix,' '},
-        {dstPortPrefix,' '}
+        {pathPrefix,NULL, NULL},
+        {srcAddrPrefix,"src host", NULL},
+        {dstAddrPrefix,"dst host", NULL},
+        {srcPortPrefix,"src port", NULL},
+        {dstPortPrefix,"dst port", NULL}
     };
 
     char *path_str;
 #if __DEBUG
     if(argc < 2){
         paramsList[0].value = "./sniffList.pcap";
-        paramsList[1].value = 0;
-        paramsList[2].value = "127.0.0.1";
-        paramsList[3].value = "42";
-        paramsList[4].value = "24";
+        paramsList[srcAddrIndex].value = "127.0.0.1";
+        paramsList[dstAddrIndex].value = "127.0.0.1";
+        paramsList[srcPortPrefix].value = "42";
+        paramsList[dstPortPrefix].value = "24";
     }
     path_str = paramsList[0].value;
 #else
@@ -71,33 +72,35 @@ int main(const int argc, const char** argv)
     path_str = strstr(argv[1], "=")+1;
 
     for(int argIt = 2; argIt < argc; ++argIt){
-        for (int paramsIt = paramsListBegin; paramsIt < paramsListEnd; ++paramsIt) {
-            if(paramsList[paramsIt].value != NULL) break;
-            paramsList[paramsIt].value = strstr(argv[argIt], "=")+1;
-            if(paramsList[paramsIt].value == NULL) continue;
+        for (int paramsIt = paramsListBegin; paramsIt < paramsListEnd+1; ++paramsIt) {
+            //если в переменной уже что-то есть то пропускаем её
+            if(paramsList[paramsIt].value != NULL) continue;
+
+            if(strstr(argv[argIt], paramsList[paramsIt].name) != 0){
+                char* value = strstr(argv[argIt], "=");
+                paramsList[paramsIt].value = strstr(argv[argIt], "=")+1;
+                break;
+            }else{
+                continue;
+            }
         }
     }
 #endif
 
-
-    FILE *path = fopen(path_str, "r");
-    if (path == NULL) {
-        printf("file %s is not exist\n", path_str);
-        return 0;
-    }else{
-        for(int it = 0; it < 3; ++it){
-            pcapFile_h[it] = pcap_open_offline(path_str, errbuf); 
+    FILE *path;
+    for(int it = 0; it < 3; ++it){
+        path = fopen(path_str, "r");
+        if (path == NULL) {
+            printf("file %s is not exist\n", path_str);
+            return 0;
         }
+        pcapFile_h[it] = pcap_open_offline(path_str, errbuf); 
         paramsList[pathIndex].value = path_str;
-        printf(".pcap successfully opened\n");
     }
+    printf(".pcap successfully opened\n");
 
     for(int it = 0; it < paramsListEnd+1; ++it){
-        char *value;
-        if(paramsList[it].value == ' ') 
-            value = "empty";
-        else
-            value = paramsList[it].value;
+        char *value = (paramsList[it].value == ' ') ? "empty" : paramsList[it].value;
         printf("%s=%s\n", paramsList[it].name, value);
     }
 
@@ -106,33 +109,40 @@ int main(const int argc, const char** argv)
 
     ///TOTAL TCP CALCULATE
     if (pcap_compile(pcapFile_h[1], &filter, filter_str, 1, PCAP_NETMASK_UNKNOWN) != 0) {
-        pcap_close(pcapFile_h);
-        printf("Couldn`t compile filter %s\n", errbuf);
+        pcap_close(pcapFile_h[1]);
+        printf("Couldn`t compile TCP filter %s\n", errbuf);
     }
     if(pcap_setfilter(pcapFile_h[1], &filter)){
-        printf("Couldn't set filter\n");
+        printf("Couldn't set TCP filter\n");
         return 0;
     } 
     int totalTCPpackets = pcap_dispatch(pcapFile_h[1], -1, ph_callback, NULL);
 
-    ///FILTERERD PACKETS CALCULATE
-    int FilteredTCPpackets;
-    if(paramsList[srcAddrIndex].value != ""){
-        if (pcap_compile(pcapFile_h[1], &filter, filter_str, 1, PCAP_NETMASK_UNKNOWN) != 0) {
-            pcap_close(pcapFile_h);
-            printf("Couldn`t compile filter %s\n", errbuf);
+    ///FILTERED PACKETS CALCULATE
+#if 1
+    for(int pos = paramsListBegin; pos < paramsListEnd+1; ++pos){
+        if(paramsList[pos].value != NULL){
+            strncat(filter_str, " and ", 5);
+            strncat(filter_str, paramsList[pos].descr , strlen(paramsList[pos].descr));
+            strncat(filter_str, " ", 1);
+            strncat(filter_str, paramsList[pos].value , strlen(paramsList[pos].value));
         }
-        if(pcap_setfilter(pcapFile_h[2], &filter)){
-            printf("Couldn't set filter\n");
-            return 0;
-        } 
-        FilteredTCPpackets= pcap_dispatch(pcapFile_h[2], -1, ph_callback, NULL);
     }
+#endif
+
+    printf("Try to find packets with filter: %s\n", filter_str);                                    
+    if (pcap_compile(pcapFile_h[2], &filter, filter_str, 1, PCAP_NETMASK_UNKNOWN) != 0) {
+        pcap_close(pcapFile_h[2]);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+        printf("Couldn`t compile filter %s\n", errbuf);
+    }
+    if(pcap_setfilter(pcapFile_h[2], &filter)){
+        printf("Couldn't set filter\n");
+        return 0;
+    } 
+    int FilteredTCPpackets = pcap_dispatch(pcapFile_h[2], -1, ph_callback, NULL);
+    
     printf("Total packets counter: %d\nTotal TCP packets counter: %d\nTotal filtered packets counter: %d\n",  totalPackets, totalTCPpackets, FilteredTCPpackets);
     printf("exiting from programm...\n");
+    close(path);
     pcap_freecode(&filter);
-
-    for(int it = 0; it < 3; ++it){
-        pcap_close(pcapFile_h[it]);
-    } 
 }
